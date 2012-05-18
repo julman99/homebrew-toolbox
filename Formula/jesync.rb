959c9ecb550da94fccb486aa30de1733cc68967d
Julio Viera <julio.viera@gmail.com>
2012-05-18 17:28:46 -0400

    md5 was wrong

--- Formula/jesync.rb
+++ Formula/jesync.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Jesync < Formula
   url 'https://github.com/downloads/julman99/JESync/JESync-0.3.zip'
   homepage ''
-  md5 '7bf7db4e77c402785bbee99cc27f1bc5'
+  md5 'b2c84c5f19c75068626c9a40d39dbe78'
   version '0.3'
 
   def install
