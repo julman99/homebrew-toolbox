78479ebdfaba32ec41bf96e1f90e6323cb7d9b75
Julio Viera <julio.viera@gmail.com>
2012-05-18 17:27:24 -0400

    v0.3

--- Formula/jesync.rb
+++ Formula/jesync.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Jesync < Formula
-  url 'https://github.com/downloads/julman99/JESync/JESync-0.2.zip'
+  url 'https://github.com/downloads/julman99/JESync/JESync-0.3.zip'
   homepage ''
-  md5 '05dc7bd2651aa958fde61ce1a748425e'
-  version '0.2'
+  md5 '7bf7db4e77c402785bbee99cc27f1bc5'
+  version '0.3'
 
   def install
 #    system 'echo "#!/bin/bash\njava -jar JESync.jar" > bin/jesync'
