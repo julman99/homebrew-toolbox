e981685ff53da423ec45c0935c90391d5b45f5d7
Julio Viera <julio.viera@gmail.com>
2013-04-20 13:01:36 -0400

    Updates jesync to version 0.7

--- Formula/jesync.rb
+++ Formula/jesync.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Jesync < Formula
-  url 'https://dl.dropbox.com/sh/7fzw435ffzge7lw/zHo3sJyP6e/JESync/JESync-0.6.zip?token_hash=AAFzhAkiUMNsV1EAupYJE_IS8k_s__2Yu--lUxqwzHQw4g&dl=1'
+  url 'https://dl.dropboxusercontent.com/sh/7fzw435ffzge7lw/T_OFYrkgpl/JESync/jesync-0.7.zip'
   homepage ''
-  sha1 '013709faaaff2cd4e43c8eedad2ea0e318c12288'
-  version '0.6'
+  sha1 '89313bf10d2f4a57ed1ff90542e61613480a9182'
+  version '0.7'
 
   def install
     prefix.install Dir['*']
