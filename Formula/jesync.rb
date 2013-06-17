40d85295645a0e653ca3f22cf94d7177010a1ca7
Julio Viera <julio.viera@gmail.com>
2013-06-17 14:20:28 -0400

    Oops, forgot to upgrade version no

--- Formula/jesync.rb
+++ Formula/jesync.rb
@@ -4,7 +4,7 @@ class Jesync < Formula
   url 'https://dl.dropboxusercontent.com/u/13722362/JESync/jesync-0.8.1.zip'
   homepage ''
   sha1 '1de015c59d5e36ad580ff1785cdd43a4919ce3eb'
-  version '0.8'
+  version '0.8.1'
 
   def install
     prefix.install Dir['*']
