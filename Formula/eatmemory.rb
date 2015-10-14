9264821a227959d31df11c1c0afa85ac97d10074
Julio Viera <julio.viera@gmail.com>
2015-10-13 20:35:54 -0400

    Update eatmemory to 0.1.5

--- Formula/eatmemory.rb
+++ Formula/eatmemory.rb
@@ -2,11 +2,12 @@ require 'formula'
 
 class Eatmemory < Formula
   homepage 'https://github.com/julman99/eatmemory'
-  url 'https://github.com/julman99/eatmemory/tarball/v0.1.2'
-  version '0.1.2'
-  sha1 'a44af4a2b716fcf4813327ddcc08b1e303683df5'
+  url 'https://github.com/julman99/eatmemory/tarball/v0.1.5'
+  version '0.1.5'
+  sha1 '116b354c446f01676e1f8dfa24eef7cd936a2964'
 
   def install
+  	system "mkdir", "#{prefix}/bin"
     system "make", "install", "PREFIX=#{prefix}"
   end
 end
